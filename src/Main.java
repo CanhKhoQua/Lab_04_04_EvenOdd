ede241eb1ca0652e2ba7daf4e4084a28175f6baa
LocNguyen <nguye2lc@mail.uc.edu>
2022-09-17 16:02:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int num=3;
+
+        if (num % 2 == 0)
+        {
+            System.out.println("The number " + num + " is an even number");
+        }
+        else
+        {
+            System.out.println("The number " + num + " is an odd number");
+        }
+
+    }
+}
